5ef13d8566acd8d19d687169aef08c4066474cdd
zhangyaowucp <zhangyaowucp@163.com>
2019-08-06 10:29:13 +0800

    Add files via upload

---
+++ theme/shownews.js
@@ -0,0 +1,68 @@
+var news = new Array();
+news[32] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; June 2019 </td><td>&nbsp; My thesis received <a href="http://www.sigecom.org/awards.html">ACM SIGecom Doctoral Dissertation Award</a> runner-up</td></tr>';
+news[31] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; May 2019 </td><td>&nbsp; My thesis received <a href="http://aamas2019.encs.concordia.ca/cfvl.html">IFAAMAS-18 Victor Lesser Distinguished Dissertation Award</a> runner up</td></tr>';
+news[30] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; May 2019 </td><td>&nbsp; Our demo on  <a href="files/Drone-demo-19.pdf">drone signaling for conservation</a> wins Best Demo Award at AAMAS 2019</td></tr>';
+news[29] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; May 2019 </td><td>&nbsp; Check our work (<a href="https://arxiv.org/pdf/1903.02917.pdf">ec’19</a>, <a href="files/attacker_deception.pdf">ijcai’19</a>) on analyzing equilibrium of learning against strategic agents</td></tr>';
+news[28] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; May 2019 </td><td>&nbsp; Check out the program of our AAMAS-19 Workshop <a href="https://jointworkshops.wixsite.com/aasg2019">Autonomous Agents for Social Good</a></td></tr>';
+news[27] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Nov 2018 </td><td>&nbsp; Invited to participate (as the only non-economists) <a href="https://cirano.qc.ca/en/events/822">Workshop on Economics of Persuasion</a></td></tr>';
+news[26] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Nov 2018 </td><td>&nbsp; Give a talk on algorithmic persuasion at Harvard EconCS seminar.</td></tr>';
+news[25] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Sep 2018 </td><td>&nbsp; I am teaching <a href="https://www.seas.harvard.edu/academics/courses/computer-science">CS 182: Artificial Intelligence </a> at Harvard this Fall</td></tr>';  
+news[24] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Aug 2018 </td><td>&nbsp; Started my postdoc at Harvard </td></tr>'; 
+news[23] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; June 2018 </td><td>&nbsp; Defended <a href="files/phd_thesis_xu.pdf">my thesis</a>! </td></tr>'; 
+news[22] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; June 2018 </td><td>&nbsp; Give a tutorial on <a href="information-ec18/index.html">Information, Persuasion and Decision Making </a> with <a href="https://www.bowaggoner.com/">Bo Waggoner</a> at <a href="http://www.sigecom.org/ec18/">EC</a> </td></tr>'; 
+news[21] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; April 2018 </td><td>&nbsp; Served on the panel about <a href="https://viterbigrad.usc.edu/instructional-support/events-workshops/phd-academic-career-mentoring-panel-series/">Preparing For, and Landing a Faculty Position</a> at USC </td></tr>'; 
+news[20] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Feb 2018 </td><td>&nbsp; <a href="https://arxiv.org/abs/1503.05988"> Algorithmic Bayesian Persuasion</a> accepted by SIAM Journal on Computing </td></tr>'; 
+news[19] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Feb 2018 </td><td>&nbsp; Co-organize a <a href="http://www.cs.cmu.edu/~noamb/aaai18/workshop.html"> Workshop on Imperfect Information Games</a> at AAAI 2018 </td></tr>';
+news[18] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Jan 2018 </td><td>&nbsp; Invited to give a talk at the <a href="http://people.hss.caltech.edu/~tamuz/negt18/"> NEGT symposium</a> at Caltech </td></tr>';
+news[17] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Jan 2018 </td><td>&nbsp; Two papers accepted to <a href="http://celweb.vuse.vanderbilt.edu/aamas18/"> AAMAS 2018</a> </td></tr>'; 
+news[16] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Jan 2018 </td><td>&nbsp; On PC of <a href="http://www.gamesec-conf.org/index.php"> GameSec 2018</a> </td></tr>';
+news[15] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Nov 2017 </td><td>&nbsp; Paper on <a href="files/sg_signal_18.pdf"> strategic coordination of patrollers and sensors </a> accepted to AAAI 2018 </td></tr>'; 
+news[14] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Oct 2017 </td><td>&nbsp; Paper <a href="https://arxiv.org/abs/1708.00611"> targeting and signaling in ad auctions</a> accepted to SODA 2018 </td></tr>'; 
+news[13] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; May 2017 </td><td>&nbsp; On PC of <a href="http://www.gamesec-conf.org/"> GameSec 2017</a> and <a href="https://aaai.org/Conferences/AAAI/aaai18.php"> AAAI 2018</a> </td></tr>'; 
+news[12] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; May 2017 </td><td>&nbsp; Invited to give a talk at the  <a href="http://itcs.shufe.edu.cn/ctw2017/"> China Theory Week</a>, Shanghai, China </td></tr>'; 
+news[11] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; May 2017 </td><td>&nbsp; Co-organize the <a href="http://teamcore.usc.edu/people/haifeng/Adverse17/index.html"> ADVERSE Workshop</a> at AAMAS 2017 <td></tr>';
+news[10] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; April 2017 </td><td>&nbsp; Honored to receive the <a href="https://research.googleblog.com/2017/04/announcing-2017-google-phd-fellows-for.html"> Google PhD fellowship</a> (<a href="https://viterbischool.usc.edu/news/2017/07/random-thoughts-haifeng-xu/">USC News</a>)  </td></tr>'; 
+news[9] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; April 2017 </td><td>&nbsp; Received the <a href="http://dornsife.usc.edu/cams/cams-prize/">Prize for Excellence in Research</a> by the USC Center for Applied Mathematics<td></tr>';
+news[8] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Nov 2016 </td><td>&nbsp; On PC of  <a href="http://ijcai-17.org/">IJCAI 2017</a> <td></tr>';
+news[7] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Nov 2016 </td><td>&nbsp; Invited to attend the <a href="https://www.cs.tau.ac.il/~mfeldman/econ-cs/program/index.php">Young Researcher Econ-CS Workshop</a> [<a href="https://www.youtube.com/watch?v=r2nQsganIBw">talk video</a>]  <td></tr>';
+news[6] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Oct 2016 </td><td>&nbsp; <a href="http://www.caltech.edu/content/linde-institutesocial-and-information-sciences-laboratory-sisl-seminar-52"> Talk   on Persuasion </a>  at <a href="http://www.sisl.caltech.edu/"> SISL, Caltech</a>  <td></tr>';
+news[5] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; July 2016 </td><td>&nbsp; Invited to give a talk on Persuasion at <a href="https://research.google.com/"> Google Research</a <td></tr>';
+news[4] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; May 2016 </td><td>&nbsp; Paper on security games and combinatorial optimization accepted by <a href="http://www.sigecom.org/ec16/"> EC 2016</a> <td></tr>'; 
+news[3] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Feb 2016 </td><td>&nbsp; Paper on Bayesian persuasion accepted by <a href="http://acm-stoc.org/stoc2016/"> STOC 2016</a> </td></tr>'; 
+news[2] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Jan 2016 </td><td>&nbsp; Three papers accepted by <a href="http://sis.smu.edu.sg/aamas2016">AAMAS 2016</a> </td></tr>'; 
+news[1] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Nov 2015 </td><td>&nbsp; On the program committee of <a href="http://ijcai-16.org">IJCAI 2016</a>  </td></tr>'; 
+news[0] = '<tr><td align=right>&nbsp;&nbsp;&nbsp; Sep 2015 </td><td>&nbsp; Visit the <a href="https://simons.berkeley.edu/programs/economics2015">Economic and Computation</a> Program at the Simons Institute until December </td></tr>'; 
+
+var maxnewscnt = 5;
+function showNews(partial) {
+  len = news.length;
+  if (partial == true) {
+    len = Math.min (news.length, maxnewscnt);
+    xtra='<tr><td></td><td>&nbsp; <a onclick="javascript:showNews(false)">More...</a></td></tr>';
+  }
+  else {
+    len = news.length;
+    xtra='<tr><td></td><td>&nbsp; <a onclick="javascript:showNews(true)">Less...</a></td></tr>';
+  }
+
+  elt = "<table border=0>";
+  for (var i=0; i < len; i++)
+    elt = elt + news[news.length-i-1] + '\n';
+  elt = elt + xtra + "\n </table>";
+  document.getElementById("news").innerHTML=elt;
+}
+
+function init() {
+  var partialnews = true;
+
+  var url = window.location.toString();
+  url.match(/\?(.+)$/);
+  var params = RegExp.$1.split("&");
+
+  for (var i=0; i <params.length; i++)
+    if (params[i] == "news")
+      partialnews = false;
+
+  showNews(partialnews);
+}
+window.onload = init; 
